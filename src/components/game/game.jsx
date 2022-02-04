62158aab20e839a0b8abf33facd803d4410dd582
norvalbv <benjinorval@gmail.com>
2022-02-04 13:12:54 +0000

    added hardmode feature

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -2,8 +2,11 @@ import "./game.scss";
 import rock from "../../images/icon-rock.svg";
 import paper from "../../images/icon-paper.svg";
 import scissors from "../../images/icon-scissors.svg";
+import spock from "../../images/icon-spock.svg";
+import lizard from "../../images/icon-lizard.svg";
 import React, { useState, useReducer, useRef } from "react";
-import mainBackground from "../../images/bg-triangle.svg";
+import easyBackground from "../../images/bg-triangle.svg";
+import hardBackground from "../../images/bg-pentagon.svg";
 
 export const Game = ({ handleWin, handleLose, gamemode }) => {
   // const reducer = (state, action) => {
@@ -28,10 +31,13 @@ export const Game = ({ handleWin, handleLose, gamemode }) => {
   const [source, setSource] = useState({ user: null, computer: null });
 
   const handleClick = (userInput) => {
-    let choiceArr = ["Rock", "Paper", "Scissors"];
-    const arr = [rock, paper, scissors];
+    let choiceArr = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+    const arr = [rock, paper, scissors, lizard, spock];
 
-    let computer = Math.floor(Math.random() * 3);
+    let computer =
+      gamemode === "easy"
+        ? Math.floor(Math.random() * 3)
+        : Math.floor(Math.random() * 5);
 
     setSource((prev) => {
       return {
@@ -50,14 +56,23 @@ export const Game = ({ handleWin, handleLose, gamemode }) => {
     });
 
     console.log(choice, source);
-
+    // spock lizard
     if (choice.user === choice.computer) {
       console.log("DRAW");
       setResult("Draw");
     } else if (
       (choice.user === "Rock" && choice.computer === "Scissors") ||
       (choice.user === "Paper" && choice.computer === "Rock") ||
-      (choice.user === "Scissors" && choice.computer === "Paper")
+      (choice.user === "Scissors" && choice.computer === "Paper") ||
+      // Hard Mode Only
+
+      (choice.user === "Scissors" && choice.computer === "Lizard") ||
+      (choice.user === "Paper" && choice.computer === "Spock") ||
+      (choice.user === "Rock" && choice.computer === "Lizard") ||
+      (choice.user === "Lizard" && choice.computer === "Spock") ||
+      (choice.user === "Lizard" && choice.computer === "Paper") ||
+      (choice.user === "Spock" && choice.computer === "Rock") ||
+      (choice.user === "Spock" && choice.computer === "Scissors")
     ) {
       console.log("win");
       handleWin();
@@ -65,7 +80,16 @@ export const Game = ({ handleWin, handleLose, gamemode }) => {
     } else if (
       (choice.user === "Rock" && choice.computer === "Paper") ||
       (choice.user === "Scissors" && choice.computer === "Rock") ||
-      (choice.user === "Paper" && choice.computer === "Scissors")
+      (choice.user === "Paper" && choice.computer === "Scissors") ||
+      // Hard Mode Only
+
+      (choice.user === "Scissors" && choice.computer === "Spock") ||
+      (choice.user === "Paper" && choice.computer === "Lizard") ||
+      (choice.user === "Rock" && choice.computer === "Spock") ||
+      (choice.user === "Lizard" && choice.computer === "Rock") ||
+      (choice.user === "Lizard" && choice.computer === "Scissors") ||
+      (choice.user === "Spock" && choice.computer === "Paper") ||
+      (choice.user === "Spock" && choice.computer === "Lizard")
     ) {
       console.log("lose");
       handleLose();
@@ -95,6 +119,7 @@ export const Game = ({ handleWin, handleLose, gamemode }) => {
                 <img src={source.user} alt="Paper Icon" />
               </button>
             </div>
+            <p>{choice.user}</p>
           </div>
           <div className="result">
             <h3 id="result-title">You {result}</h3>
@@ -103,7 +128,13 @@ export const Game = ({ handleWin, handleLose, gamemode }) => {
                 setResult("");
               }}
               id="play-again"
-              style={result === "Lose" ? { color: "red" } : { color: "green" }}
+              style={
+                result === "Lose"
+                  ? { color: "red" }
+                  : result === "Win"
+                  ? { color: "green" }
+                  : null
+              }
             >
               Play again
             </button>
@@ -121,66 +152,79 @@ export const Game = ({ handleWin, handleLose, gamemode }) => {
                   result === "Lose" ? "game-button win" : "game-button"
                 }
               >
-                <img src={source.computer} alt={""} />
+                <img src={source.computer} alt="Computer choice" />
               </button>
             </div>
+            <p>{choice.computer}</p>
           </div>
         </div>
       ) : (
         <>
-          <img src={mainBackground} alt="" className="background" />
-
+          {gamemode === "easy" ? (
+            <img src={easyBackground} alt="" className="background" />
+          ) : (
+            <img src={hardBackground} alt="" className="background" />
+          )}
           <div className="icons">
-            <div className="grad-wrap paper-wrap">
-              <button
-                onClick={() => handleClick("Paper")}
-                id="paper"
-                className="game-button"
-              >
-                <img src={paper} alt="Paper Icon" />
-              </button>
+            <div className="icon-3d-display paper-3d">
+              <span className="grad-wrap paper-wrap">
+                <button
+                  onClick={() => handleClick("Paper")}
+                  id="paper"
+                  className="game-button"
+                >
+                  <img src={paper} alt="Paper Icon" />
+                </button>
+              </span>
             </div>
-            <div className="grad-wrap rock-wrap" id="rock-placement">
-              <button
-                onClick={() => handleClick("Rock")}
-                id="rock"
-                className="game-button"
-              >
-                <img src={rock} alt="Rock Icon" />
-              </button>
+            <div className="icon-3d-display rock-3d">
+              <span className="grad-wrap rock-wrap" id="rock-placement">
+                <button
+                  onClick={() => handleClick("Rock")}
+                  id="rock"
+                  className="game-button"
+                >
+                  <img src={rock} alt="Rock Icon" />
+                </button>
+              </span>
             </div>
-            <div className="grad-wrap scissors-wrap">
-              <button
-                onClick={() => handleClick("Scissors")}
-                id="scissors"
-                className="game-button"
-              >
-                <img src={scissors} alt="Scissors Icon" />
-              </button>
-
-              {gamemode === "hard" && (
-                <div className="grad-wrap scissors-wrap">
+            <div className="icon-3d-display scissors-3d">
+              <span className="grad-wrap scissors-wrap">
+                <button
+                  onClick={() => handleClick("Scissors")}
+                  id="scissors"
+                  className="game-button"
+                >
+                  <img src={scissors} alt="Scissors Icon" />
+                </button>
+              </span>
+            </div>
+            {gamemode === "hard" && (
+              <div className="icon-3d-display lizard-3d">
+                <span className="grad-wrap lizard-wrap">
                   <button
-                    onClick={() => handleClick("Scissors")}
-                    id="scissors"
+                    onClick={() => handleClick("Lizard")}
+                    id="lizard"
                     className="game-button"
                   >
-                    <img src={scissors} alt="Scissors Icon" />
+                    <img src={lizard} alt="Lizard Icon" />
                   </button>
-                </div>
-              )}
-              {gamemode === "hard" && (
-                <div className="grad-wrap scissors-wrap">
+                </span>
+              </div>
+            )}
+            {gamemode === "hard" && (
+              <div className="icon-3d-display spock-3d">
+                <span className="grad-wrap spock-wrap">
                   <button
-                    onClick={() => handleClick("Scissors")}
-                    id="scissors"
+                    onClick={() => handleClick("Spock")}
+                    id="spock"
                     className="game-button"
                   >
-                    <img src={scissors} alt="Scissors Icon" />
+                    <img src={spock} alt="Spock Icon" />
                   </button>
-                </div>
-              )}
-            </div>
+                </span>
+              </div>
+            )}
           </div>
         </>
       )}
